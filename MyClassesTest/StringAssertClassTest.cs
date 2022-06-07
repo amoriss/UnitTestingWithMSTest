ae0ad4466dc5da65772c66ab092ba76065801477
Amoriss <amoriss@protonmail.com>
2022-06-06 23:11:50 -0500

    Using the StringAssert Class

---
+++ MyClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest : TestBase
+    {
+        [TestMethod]
+        public void ContainsTest()
+        {
+            string str1 = "Steve Nystrom";
+            string str2 = "Nystrom";
+
+            //string 1 contains string 2
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        public void StartsWithTest()
+        {
+            string str1 = "All Lower Case";
+            string str2 = "All Lower";
+
+            //string 1 starts with string 2
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        public void IsAllLowerCaseTest()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("all lower case", r);
+        }
+
+        [TestMethod]
+        public void IsNotAllLowerCaseTest()
+        {
+            Regex r = new(@"^([^A-Z]+$)");
+
+            StringAssert.DoesNotMatch("All Lower Case", r);
+        }
+    }
+}
